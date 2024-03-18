cdfd3ed8884044ff87eb8fbf9d82dce90114858b
gaganparmar2003 <gaganparmar59@gmail.com>
2024-03-19 09:23:57 +1300

    Login Page Added

--- studio3code/src/TopBar.js
+++ studio3code/src/TopBar.js
@@ -1,26 +1,61 @@
 // src/TopBar.js
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
-const TopBar = () => {
+const TopBar = ({ loggedInUser }) => {
+  const navigate = useNavigate();
+
   return (
-    <header>
-      <nav className="top-bar">
-        <Link to="/" className="menu-button">
-          <div className="button-inner">
-            <span>Home</span>
+    <header className="bg-dark text-white">
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div className="container-fluid">
+          <Link to="/" className="navbar-brand">
+            Dumplings Shop
+          </Link>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav ms-auto">
+              <li className="nav-item">
+                <Link to="/" className="nav-link">
+                  Home
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link to="/dumplings" className="nav-link">
+                  Dumplings
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link to="/cart" className="nav-link">
+                  Cart
+                </Link>
+              </li>
+              {!loggedInUser ? (
+                <li className="nav-item">
+                  <Link to="/login" className="nav-link">
+                    Login
+                  </Link>
+                </li>
+              ) : (
+                <li className="nav-item">
+                  <Link to="/member" className="nav-link">
+                    Member
+                  </Link>
+                </li>
+              )}
+            </ul>
           </div>
-        </Link>
-        <Link to="/dumplings" className="menu-button">
-          <div className="button-inner">
-            <span>Dumplings</span>
-          </div>
-        </Link>
-        <Link to="/cart" className="menu-button">
-          <div className="button-inner">
-            <span>Cart</span>
-          </div>
-        </Link>
+        </div>
       </nav>
     </header>
   );
