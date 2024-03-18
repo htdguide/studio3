2e9c0b856cf5ae0e93e00229b575b4bfa20a59ff
sahil kakadiya <sahilkakadiya2018@gmail.com>
2024-03-19 09:31:17 +1300

    Login Page Added

--- studio3code/src/DumplingsMenu.js
+++ studio3code/src/DumplingsMenu.js
@@ -1,12 +1,9 @@
-// src/DumplingsMenu.js
 import React from 'react';
 
 const DumplingsMenu = () => {
-  // Dummy data for dumplings
   const dumplingsData = [
     { id: 1, name: 'Dumpling 1', price: 5, image: 'dumpling1.jpg' },
     { id: 2, name: 'Dumpling 2', price: 6, image: 'dumpling2.jpg' },
-    // Add more dumplings as needed
   ];
 
   return (
